f443547199f916fccbb75318a9d60da4ba81615c
Yesterday17 <t@yesterday17.cn>
2018-06-19 00:19:11 +0800

    完成前端网站

---
+++ json/format.js
@@ -0,0 +1,14 @@
+export let formats = [
+  {
+    modid: "silentgems",
+    key: "item.silentgems:ChaosOrb.break",
+    en_us: "Your %1$s shatters into %2$,d pieces.",
+    zh_cn: "你的%1$s破碎了，碎裂成%2$s件。"
+  },
+  {
+    modid: "minecraft",
+    key: "item.apple.name",
+    en_us: "Your %1$s apple.",
+    zh_cn: "你的%1$s苹果"
+  }
+];
