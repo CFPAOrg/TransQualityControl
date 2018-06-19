50f2a936e3683672d110de69130cc14c0eab2a74
Yesterday17 <t@yesterday17.cn>
2018-06-19 14:35:08 +0800

    修复文件定位问题

--- js/index.js
+++ js/index.js
@@ -1,40 +1,32 @@
-import {
-    formats
-} from "./js/format.js"
-import {
-    duplicates
-} from "./js/duplicate.js"
-import {
-    branches
-} from "./js/branch.js"
-import {
-    times
-} from "./js/time.js"
+import { formats } from "./format.js";
+import { duplicates } from "./duplicate.js";
+import { branches } from "./branch.js";
+import { times } from "./time.js";
 
 let format = new Vue({
-    el: "#format",
-    data: {
-        formats: formats
-    }
+  el: "#format",
+  data: {
+    formats: formats
+  }
 });
 
 let duplicate = new Vue({
-    el: "#duplicate",
-    data: {
-        duplicates: duplicates
-    }
-})
+  el: "#duplicate",
+  data: {
+    duplicates: duplicates
+  }
+});
 
 let branch = new Vue({
-    el: "#branch",
-    data: {
-        branches: branches
-    }
-})
+  el: "#branch",
+  data: {
+    branches: branches
+  }
+});
 
 let time = new Vue({
-    el: "#time",
-    data: {
-        times: times
-    }
-})
+  el: "#time",
+  data: {
+    times: times
+  }
+});
