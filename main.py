5f9350f3e1bb7c0e542b5c1516f1a8c1f59ded5a
TartaricAcid <baka943@qq.com>
2018-06-19 14:58:59 +0800

    添加 Minecraft 语言文件作为判定文件 添加时间部分动态显示

--- main.py
+++ main.py
@@ -5,31 +5,32 @@ import src.format_char_check.format_main
 
 import sys
 import os
+import time
+import json
 
 if __name__ == '__main__':
     def info_w():
         branch_json = src.branch_error_check.branch_main.branch_check(project_assets)
         duplicate_json = src.duplicate_key_check.duplicate_main.duplicate_main(project_assets)
         format_json = src.format_char_check.format_main.format_main(project_assets)
+        time_json = time_gen()
 
-        if (os.path.exists("./branch.js") == True):
-            os.remove("./branch.js")
-        if (os.path.exists("./duplicate.js") == True):
-            os.remove("./duplicate.js")
-        if (os.path.exists("./format.js") == True):
-            os.remove("./format.js")
-
-        # 删除文件
+        # 重写文件
         branch = open("./branch.js", "w", encoding="utf-8")
         duplicate = open("./duplicate.js", "w", encoding="utf-8")
-        formatjson = open("./format.js", "w", encoding="utf-8")
+        format = open("./format.js", "w", encoding="utf-8")
+        time = open("./time.js", "w", encoding="utf-8")
+
+        # 写 js 数据到文件
         branch.write("export let branches=" + branch_json + ";")
         duplicate.write("export let duplicates=" + duplicate_json + ";")
-        formatjson.write("export let formats=" + format_json + ";")
+        format.write("export let formats=" + format_json + ";")
+        time.write("export let times=" + time_json + ";")
+
         branch.close()
         duplicate.close()
-        formatjson.close()
-        # 写json数据到文件
+        format.close()
+        time.close()
 
 
     def setProjectDir(dir):
@@ -37,6 +38,11 @@ if __name__ == '__main__':
         global project_assets
         project_dir = dir
         project_assets = project_dir + "/assets"
+
+
+    def time_gen():
+        dict_time = {"time": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}
+        return json.dumps(dict_time, ensure_ascii=False)
 
 
     syslist = sys.argv
