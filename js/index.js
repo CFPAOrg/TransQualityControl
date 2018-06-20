b948efc17c7eeda75e3d277c6c9e40551b8c86cd
TartaricAcid <baka943@qq.com>
2018-06-20 14:42:55 +0800

    添加路径检查，特定词语检查

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 import { formats } from "./format.js";
 import { duplicates } from "./duplicate.js";
 import { branches } from "./branch.js";
+import { dirs } from "./dir.js";
+import { words } from "./word.js";
 import { times } from "./time.js";
 
 let format = new Vue({
@@ -21,6 +23,20 @@ let branch = new Vue({
   el: "#branch",
   data: {
     branches: branches
+  }
+});
+
+let dir = new Vue({
+  el: "#dir",
+  data: {
+    dirs: dirs
+  }
+});
+
+let word = new Vue({
+  el: "#word",
+  data: {
+    words: words
   }
 });
 
